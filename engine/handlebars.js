4e97b6a7fa89048c75005065f62537d63c6c70fc
Emiliano Gonzalez <egonzalez@ipxon.com>
2022-02-12 22:12:12 +0000

    cambios para soportar pug y mejoras en el código.

---
+++ engine/handlebars.js
@@ -0,0 +1,11 @@
+const { engine } = require('express-handlebars')
+
+module.exports = function(app) {
+    app.engine('hbs', engine({
+        layoutDir: path.join(__dirname, 'views/layouts'),
+        defaultLayout: 'index',
+        extname: 'hbs'
+    }))
+    app.set('views', './views/')
+    app.set('view engine', 'hbs');
+}
--- server.js
+++ server.js
@@ -3,73 +3,23 @@ const path = require('path')
 
 const PORT = process.env.port || 8080
 
-//const Contenedor = require('./models/productos')
-//const prod = new Contenedor("productos.txt")
-
 const productosRouter = require('./routes/productos')
-
-const { engine } = require('express-handlebars')
+const pugEngine = require('./engine/pug')
 
 const app = express()
 
-//app.use(express.json())
-//app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.engine('hbs', engine({
-  layoutDir: path.join(__dirname, 'views/layouts'),
-  defaultLayout: 'index',
-  extname: 'hbs'
-}))
-app.set('view engine', 'hbs')
+pugEngine(app)
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-
-app.get('/', async(req, res) => {
-  //const productos = await prod.getAll()
-  //console.log(pr)
-  //res.render("productos", { productos, name: 'Emiliano'})
+app.get('/', (req, res) => {
   res.render('main')
 })
 
 app.use('/productos', productosRouter)
-
-/*
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-
-app.get('/productos', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/productos.html'))
-})
-*/
-
-/*
-app.get('/productos', async(req, res, ) => {
-  const listado = await productos.getAll()
-  .then(result => {
-    return result
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(500)
-  })
-  res.send(JSON.stringify(listado)).status(200)
-})
-
-app.get('/productorandom', async(req, res, ) => {
-  const listado = await productos.getAll()
-  .then(result => {
-    return result
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(500)
-  })
-  const item = listado[Math.floor(Math.random()*listado.length)]
-  res.send(JSON.stringify(item)).status(200)
-})
-*/
 
 const server = app.listen(PORT, function(){
   console.log(`Servidor iniciando en port ${PORT}`)
